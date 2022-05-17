232557e2258fd4903ed1d45c784ebad9692ce7b4
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2022-05-17 17:12:09 +0200

    Ejercicio de condiciones PHP

---
+++ PHP/ejercio-condicionales-color.php
@@ -0,0 +1,25 @@
+<?php
+$color = "azul";
+?>
+<?php if($color == "azul"){ ?>
+	<!DOCTYPE html>
+	<html lang="es">
+		<head>
+			<title>Ejercicio de color azul</title>
+		</head>
+		<body>
+			<h1>Color azul escogido</h1>
+		</body>
+	</html>
+<?php } ?>
+<?php if($color == "rojo"){ ?>
+	<!DOCTYPE html>
+	<html lang="es">
+		<head>
+			<title>Ejercicio de color rojo</title>
+		</head>
+		<body>
+			<h1>Color rojo escogido</h1>
+		</body>
+	</html>
+<?php } ?>
