1fdef6fdb798cc65f9a918fb3495dc7b36cc364d
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2022-05-23 17:09:37 +0200

    Segundo ejemplo require

---
+++ PHP/pagina2.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h2>Subtitular <?php echo $variable; ?></h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis temporibus tempora necessitatibus aspernatur animi, eius reiciendis eveniet. Fugit, magnam nemo! Omnis, earum. Error beatae accusamus adipisci quaerat fugit, voluptatum harum.</p>
+</body>
+</html>
