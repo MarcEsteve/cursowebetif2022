f2b8ff1f2e3e8df0099508fb6fc450d68ab9fc96
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2022-07-20 15:09:49 +0200

    pequeño cambio

--- PHP/mysql/pdo_pruebas.php
+++ PHP/mysql/pdo_pruebas.php
@@ -3,7 +3,7 @@
 try {
 	$conexion = new PDO('mysql:host=localhost;dbname=heidisql_curso', 'root', '');
 	echo "Conexion OK";
-}catch(PDOException $excepciones){
+} catch(PDOException $excepciones){
 	echo "Error: " . $excepciones->getMessage();
 }
 
